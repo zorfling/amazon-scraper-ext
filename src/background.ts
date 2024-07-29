28fdb789dc50a503c4891f6cde7dbe34dd8a30b1
Chris Colborne <zorfling@gmail.com>
2024-07-29 11:07:34 +1000

    Convert to promise based and download file at end

--- src/background.ts
+++ src/background.ts
@@ -55,161 +55,178 @@ function parseOrders() {
     'Gift Message',
     'Gift Sender Name',
     'Gift Recipient Contact Details',
-    'Vendor'
+    'Vendor',
   ] as const;
 
   type Header = (typeof headers)[number];
   let csvString = headers.join(',') + '\n';
 
-  orderIds.forEach((orderId) => {
-    getOrderDetails(orderId).then((response) => {
-      response.text().then((responseBody) => {
-        const doc = new DOMParser().parseFromString(responseBody, 'text/html');
-
-        const items = doc.querySelectorAll('.a-fixed-left-grid');
-
-        const trackingLink =
-          doc.querySelector('.track-package-button a')?.getAttribute('href') ||
-          '';
-
-        const itemNumbers = items.length;
-
-        const subTotals = (doc.querySelector('#od-subtotals') as HTMLElement)
-          .innerText;
-
-        let shipping = '';
-        if (subTotals) {
-          const matches = subTotals.match(
-            /Shipping & Handling:\s*(\$|USD)\s*(.*)/
+  Promise.all(
+    orderIds.map((orderId) => {
+      return getOrderDetails(orderId).then((response) => {
+        return response.text().then((responseBody) => {
+          const doc = new DOMParser().parseFromString(
+            responseBody,
+            'text/html'
           );
-          if (matches && matches[2]) {
-            shipping = matches[2];
+
+          const items = doc.querySelectorAll('.a-fixed-left-grid');
+
+          const trackingLink =
+            doc
+              .querySelector('.track-package-button a')
+              ?.getAttribute('href') || '';
+
+          const itemNumbers = items.length;
+
+          const subTotals = (doc.querySelector('#od-subtotals') as HTMLElement)
+            .innerText;
+
+          let shipping = '';
+          if (subTotals) {
+            const matches = subTotals.match(
+              /Shipping & Handling:\s*(\$|USD)\s*(.*)/
+            );
+            if (matches && matches[2]) {
+              shipping = matches[2];
+            }
           }
-        }
 
-        const shippingPer = (parseFloat(shipping) * 100) / itemNumbers / 100;
+          const shippingPer = (parseFloat(shipping) * 100) / itemNumbers / 100;
 
-        const orderDateMatches = (
-          doc.querySelector('.order-date-invoice-item') as HTMLElement
-        ).innerText.match('Ordered on (.*)');
+          const orderDateMatches = (
+            doc.querySelector('.order-date-invoice-item') as HTMLElement
+          ).innerText.match('Ordered on (.*)');
 
-        const orderDateString = orderDateMatches ? orderDateMatches[1] : '';
-        const orderDate = new Date(orderDateString);
-        orderDate.setHours(10);
+          const orderDateString = orderDateMatches ? orderDateMatches[1] : '';
+          const orderDate = new Date(orderDateString);
+          orderDate.setHours(10);
 
-        const orderDateISO = orderDate.toISOString();
+          const orderDateISO = orderDate.toISOString();
 
-        const isEasyBookPrep = (
-          doc.querySelector(
-            '.od-shipping-address-container .displayAddressDiv'
-          ) as HTMLElement
-        ).innerText.includes('POLARIS');
+          const isEasyBookPrep = (
+            doc.querySelector(
+              '.od-shipping-address-container .displayAddressDiv'
+            ) as HTMLElement
+          ).innerText.includes('POLARIS');
 
-        Array.from(items).forEach((item) => {
-          const productTitleElement =
-            item.querySelectorAll('.a-link-normal')[1];
-          const productTitle =
-            (productTitleElement as HTMLElement)?.innerText || '';
+          const orderPromises = Array.from(items).map((item) => {
+            const productTitleElement =
+              item.querySelectorAll('.a-link-normal')[1];
+            const productTitle =
+              (productTitleElement as HTMLElement)?.innerText || '';
 
-          const price = parseFloat(
-            trim(
-              (item.querySelector('.a-color-price') as HTMLElement)?.innerText
-            ) || '0'
-          );
+            const price = parseFloat(
+              trim(
+                (item.querySelector('.a-color-price') as HTMLElement)?.innerText
+              ) || '0'
+            );
 
-          const priceCents = Math.floor(price * 100);
-          const shippingCents = Math.floor(shippingPer * 100);
+            const priceCents = Math.floor(price * 100);
+            const shippingCents = Math.floor(shippingPer * 100);
 
-          const totalOwed = (priceCents + shippingCents) / 100;
+            const totalOwed = (priceCents + shippingCents) / 100;
 
-          // trim whitespace and newlines from ends
-          const productTitleTrimmed = productTitle.replace(/^\s+|\s+$/g, '');
+            // trim whitespace and newlines from ends
+            const productTitleTrimmed = productTitle.replace(/^\s+|\s+$/g, '');
 
-          const productLink = productTitleElement?.getAttribute('href') || '';
+            const productLink = productTitleElement?.getAttribute('href') || '';
 
-          const asin = productLink?.split('/')[3];
+            const asin = productLink?.split('/')[3];
 
-          const orderDetails: Record<Header, any> = {
-            Website: 'zzAmazon.com',
-            'Order ID': orderId,
-            'Order Date': orderDateISO,
-            'Purchase Order Number': 'zzNot Applicable',
-            Currency: 'USD',
-            'Unit Price': totalOwed,
-            'Unit Price Tax': 'zztax',
-            'Shipping Charge': 'zz0',
-            'Total Discounts': 'zz0',
-            'Total Owed': totalOwed,
-            'Shipment Item Subtotal': totalOwed,
-            'Shipment Item Subtotal Tax': 'zztax',
-            ASIN: asin,
-            'Product Condition': 'condition',
-            Quantity: '1',
-            'Payment Instrument Type': 'zzVisa',
-            'Order Status': 'zzFor sale',
-            'Shipment Status': 'zzShipped',
-            'Ship Date': 'zzshipdate',
-            'Shipping Option': 'zzStandard',
-            'Shipping Address': isEasyBookPrep ? 'POLARIS' : 'Little Owl',
-            'Billing Address': 'zzaddress',
-            'Carrier Name & Tracking Number': 'zz',
-            'Product Name': `"${productTitleTrimmed}"`,
-            'Gift Message': 'zz',
-            'Gift Sender Name': 'zz',
-            'Gift Recipient Contact Details': 'zz',
-            Vendor: 'vendor'
-          };
+            const orderDetails: Record<Header, any> = {
+              Website: 'zzAmazon.com',
+              'Order ID': orderId,
+              'Order Date': orderDateISO,
+              'Purchase Order Number': 'zzNot Applicable',
+              Currency: 'USD',
+              'Unit Price': totalOwed,
+              'Unit Price Tax': 'zztax',
+              'Shipping Charge': 'zz0',
+              'Total Discounts': 'zz0',
+              'Total Owed': totalOwed,
+              'Shipment Item Subtotal': totalOwed,
+              'Shipment Item Subtotal Tax': 'zztax',
+              ASIN: asin,
+              'Product Condition': 'condition',
+              Quantity: '1',
+              'Payment Instrument Type': 'zzVisa',
+              'Order Status': 'zzFor sale',
+              'Shipment Status': 'zzShipped',
+              'Ship Date': 'zzshipdate',
+              'Shipping Option': 'zzStandard',
+              'Shipping Address': isEasyBookPrep ? 'POLARIS' : 'Little Owl',
+              'Billing Address': 'zzaddress',
+              'Carrier Name & Tracking Number': 'zz',
+              'Product Name': `"${productTitleTrimmed}"`,
+              'Gift Message': 'zz',
+              'Gift Sender Name': 'zz',
+              'Gift Recipient Contact Details': 'zz',
+              Vendor: 'vendor',
+            };
 
-          getInvoiceDetails(orderId).then((invResponse) => {
-            invResponse.text().then((invResponseBody) => {
-              const conditionMatches =
-                invResponseBody.match(/Condition:\s*(.*)</);
-              const condition = conditionMatches ? conditionMatches[1] : '';
+            return getInvoiceDetails(orderId).then((invResponse) => {
+              return invResponse.text().then((invResponseBody) => {
+                const conditionMatches =
+                  invResponseBody.match(/Condition:\s*(.*)</);
+                const condition = conditionMatches ? conditionMatches[1] : '';
 
-              const vendorMatches =
-                invResponseBody.match(/Sold by:\s*(.*)\s*\(/);
-              const vendor = vendorMatches ? vendorMatches[1] : '';
+                const vendorMatches =
+                  invResponseBody.match(/Sold by:\s*(.*)\s*\(/);
+                const vendor = vendorMatches ? vendorMatches[1] : '';
 
-              orderDetails['Product Condition'] = condition;
-              orderDetails['Vendor'] = `"${trim(vendor)}"`;
+                orderDetails['Product Condition'] = condition;
+                orderDetails['Vendor'] = `"${trim(vendor)}"`;
 
-              getTrackingInfo(trackingLink).then((trackingInfoResponse) => {
-                trackingInfoResponse.text().then((trackingInfoResponseBody) => {
-                  const trackingInfoDoc = new DOMParser().parseFromString(
-                    trackingInfoResponseBody,
-                    'text/html'
-                  );
+                return getTrackingInfo(trackingLink).then(
+                  (trackingInfoResponse) => {
+                    return trackingInfoResponse
+                      .text()
+                      .then((trackingInfoResponseBody) => {
+                        const trackingInfoDoc = new DOMParser().parseFromString(
+                          trackingInfoResponseBody,
+                          'text/html'
+                        );
 
-                  const trackingNumber =
-                    (
-                      trackingInfoDoc.querySelector(
-                        '.pt-delivery-card-trackingId'
-                      ) as HTMLElement
-                    )?.innerText ?? '';
+                        const trackingNumber =
+                          (
+                            trackingInfoDoc.querySelector(
+                              '.pt-delivery-card-trackingId'
+                            ) as HTMLElement
+                          )?.innerText ?? '';
 
-                  const trackingNumberOnly =
-                    trackingNumber.match(/Tracking ID: (.*)/);
+                        const trackingNumberOnly =
+                          trackingNumber.match(/Tracking ID: (.*)/);
 
-                  orderDetails['Carrier Name & Tracking Number'] =
-                    trackingNumberOnly?.[1] ?? '';
+                        orderDetails['Carrier Name & Tracking Number'] =
+                          trackingNumberOnly?.[1] ?? '';
 
-                  // print as csv
-                  csvString +=
-                    headers
-                      .map((key) => orderDetails[key as Header])
-                      .join(',') + '\n';
-                  // console.log(
-                  //   headers.map((key) => orderDetails[key as Header]).join(',') + '\n'
-                  // );
-
-                  console.log(csvString);
-                });
+                        // print as csv
+                        csvString +=
+                          headers
+                            .map((key) => orderDetails[key as Header])
+                            .join(',') + '\n';
+                      });
+                  }
+                );
               });
             });
           });
+
+          return Promise.all(orderPromises);
         });
       });
-    });
+    })
+  ).then(() => {
+    console.log(csvString);
+    const blob = new Blob([csvString], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = 'orders.csv';
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
   });
 }
 
@@ -220,7 +237,7 @@ chrome.action.onClicked.addListener((tab) => {
   if (!tab.url.includes('chrome://')) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      func: parseOrders
+      func: parseOrders,
     });
   }
 });
